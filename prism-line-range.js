727565f472a5615696195a4a006d7b695e5c2f22
atomize <63r71@tuta.io>
2019-03-14 18:19:29 -0500

    added comments to the main file

--- prism-line-range.js
+++ prism-line-range.js
@@ -2,7 +2,11 @@
     if (typeof self === 'undefined' || !self.Prism || !self.document || !document.querySelector) {
         return;
     }
-
+    
+    /**
+     * Fetches the file from a local or remote URL string and returns text
+     * @param  {string} url - The url or file path of the source code to fetch
+     */
     function fetchText(url) {
         const response = fetch(url).then(response => {
             return response.text()
@@ -10,6 +14,11 @@
         return response
     }
 
+    /**
+     * Fetches the text of the provided URL/file and returns an Array with the name of the file and the source code as text 
+     * ['sourcefileName.js', sourceCode]
+     * @param  {string} sourceURL - The url or file path of the source code to send to fetchText()
+     */
     function init(sourceURL) {
         return fetchText(sourceURL).then(
             x => {
@@ -18,6 +27,11 @@
         )
     }
 
+    /**
+     * Takes a PRE element, figures out what file it is associated with, get the desired line range, and splits the source text and injects it highlighted
+     * @param  {DOM element} pre - A DOM element, in this case will be a <pre>
+     * @param  {array} s - An array with the source code file name and the source code contents as text (from init())
+     */
     function splitLines(pre, s) {
         var lineRange = pre.getAttribute("data-range")
         var rawLines = lineRange.split(',')
@@ -30,7 +44,11 @@
         pre.innerHTML = codeRangeEl
         Prism.highlightAllUnder(pre)
     }
-
+    
+    /**
+     * This is the only function that needs to be run by Prism. It get the appropriate elements from the DOM
+     * figures out the unique list of files to be fetched, and sends them to be fetched and cached, each only once.
+     */
     function lineRange() {
         var tutorialElements = Array.prototype.slice.apply(document.querySelectorAll("pre[data-fetch]"))
         var fileArray = tutorialElements.map(el => el.getAttribute('data-fetch'))
@@ -43,6 +61,10 @@
             })
         }))
     }
+
+    /**
+     * Prism.js hooks for registering the plugin with their API
+     */
     Prism.hooks.add('line-range', function (env) {
         env.plugins = env.plugins || {};
         env.plugins.linerange = true;
@@ -51,6 +73,13 @@
     Prism.plugins.linerange = {
         lineRange: lineRange
     }
+
+    /**
+     * We need to wait until the DOM is loaded so we can get a list of files we want.
+     * This is going to preform well, although there would be ways to listen to the DOM loading and 
+     * IMMEDIATELY fetch the files as they became known rather than waiting for the whole document to load.
+     * Performance improvements here would be negligible unless the DOM get really big
+     */
     if (document.readyState === "loading") { // Loading hasn't finished yet
         document.addEventListener("DOMContentLoaded", lineRange);
     } else { // `DOMContentLoaded` has already fired
