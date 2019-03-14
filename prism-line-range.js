75fea42768b03c190f0f86cf5c1b939c201788a0
atomize <63r71@tuta.io>
2019-03-14 17:56:10 -0500

    oops

--- prism-line-range.js
+++ prism-line-range.js
@@ -2,13 +2,14 @@
     if (typeof self === 'undefined' || !self.Prism || !self.document || !document.querySelector) {
         return;
     }
-   
+
     function fetchText(url) {
         const response = fetch(url).then(response => {
             return response.text()
         });
         return response
     }
+
     function init(sourceURL) {
         return fetchText(sourceURL).then(
             x => {
@@ -16,6 +17,7 @@
             }
         )
     }
+
     function splitLines(pre, s) {
         var lineRange = pre.getAttribute("data-range")
         var rawLines = lineRange.split(',')
@@ -23,11 +25,12 @@
         var startLine = parseInt(lines[0], 10)
         var endLine = lines[1] === undefined ? -1 : parseInt(lines[1], 10)
         var codeRange = s[1].slice(startLine - 1, endLine).join('\n')
-        var codeRangeEl = '<code class="line-numbers lang-js">'+codeRange.trim()+'</code>'
-        !pre.getAttribute("data-start") ? pre.setAttribute('data-start', startLine): null
+        var codeRangeEl = '<code class="line-numbers lang-js">' + codeRange.trim() + '</code>'
+        !pre.getAttribute("data-start") ? pre.setAttribute('data-start', startLine) : null
         pre.innerHTML = codeRangeEl
         Prism.highlightAllUnder(pre)
     }
+
     function lineRange() {
         var tutorialElements = Array.prototype.slice.apply(document.querySelectorAll("pre[data-fetch]"))
         var fileArray = tutorialElements.map(el => el.getAttribute('data-fetch'))
@@ -49,8 +52,8 @@
         lineRange: lineRange
     }
     if (document.readyState === "loading") { // Loading hasn't finished yet
-    document.addEventListener("DOMContentLoaded", lineRange);
-  } else { // `DOMContentLoaded` has already fired
-    lineRange();
-  }
-    })();
+        document.addEventListener("DOMContentLoaded", lineRange);
+    } else { // `DOMContentLoaded` has already fired
+        lineRange();
+    }
+})();
